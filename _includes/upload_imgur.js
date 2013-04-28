e70afc1aa0c7d71620381c7c68c87570d768bd7a
Roberto Covarrubias <maner.moniar@gmail.com>
2013-04-27 21:09:51 -0500

    Pruebas...

--- _includes/upload_imgur.js
+++ _includes/upload_imgur.js
@@ -9,12 +9,13 @@ function upload(file) {
     /* It is! */
     /*document.body.className = "uploading";*/
 	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "https://api.imgur.com/oauth2/authorize?client_id=5bd2102038b4c62&response_type=token");
+	xhr.open("POST", "https://api.imgur.com/oauth2/authorize?client_id=eac34bd7408ece5&response_type=token");
 	
-	
+	console.log('despues POST');
 	xhr.onreadystatechange = function (e) {
 	  if (xhr.readyState == 4) {
 		if(xhr.status == 200){
+			console.log('200');
 			/* Lets build a FormData object*/
 			var fd = new FormData(); // I wrote about it: https://hack111019e1b70421e1d217666cf1f8dac6b9dc2c87s.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
 			fd.append("image", file); // Append the file
@@ -26,7 +27,7 @@ function upload(file) {
 			
 			xhr2.onload = function() {
 				// Big win!
-						
+				console.log('onload');		
 				var link = JSON.parse(xhr2.responseText).data.link;
 				
 				var link_m = link.replace(/(\.[a-zA-Z]{3})$/g,"m$1")
