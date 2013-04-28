df7428911debb07184343dbeaebe662cf20ca900
Roberto Covarrubias <maner.moniar@gmail.com>
2013-04-28 01:14:40 -0500

    Pruebas...

--- _includes/upload_imgur.js
+++ _includes/upload_imgur.js
@@ -4,10 +4,10 @@ window.ondrop = function(e) {
 		e.preventDefault(); 
 		//upload(e.dataTransfer.files[0]); 
 		
-		var client_id = "162f8664c190969"
-		var client_secret = "ed9ff3c498b9d9ad1c54c867a65024747e77009e"
+		var client_id = "162f8664c190969";
+		var client_secret = "ed9ff3c498b9d9ad1c54c867a65024747e77009e";
 		
-		//getPin(client_id);
+		getPin(client_id);
 		
 }
 
