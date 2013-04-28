2806fdb2270d941962019cea02c1135bf045e2b8
Roberto Covarrubias <maner.moniar@gmail.com>
2013-04-27 19:58:40 -0500

    Pruebas...

--- _includes/upload_imgur.js
+++ _includes/upload_imgur.js
@@ -14,7 +14,7 @@ function upload(file) {
     fd.append("image", file); // Append the file
     var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
     //xhr.setRequestHeader('Authorization:','Client-ID eac34bd7408ece5');
-    xhr.open("POST", "http://api.imgur.com/3/gallery/album/'prueba'"); // Boooom!
+    xhr.open("POST", "https://ljzac.imgur.com/3/image.json"); // Boooom!
 	
 
     xhr.onload = function() {
