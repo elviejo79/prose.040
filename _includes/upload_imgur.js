f1372c35b2c8262d17878331a13c77ece8696856
Roberto Covarrubias <maner.moniar@gmail.com>
2013-04-27 20:25:59 -0500

    Pruebas...

--- _includes/upload_imgur.js
+++ _includes/upload_imgur.js
@@ -14,7 +14,7 @@ function upload(file) {
     fd.append("image", file); // Append the file
     var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
     //xhr.setRequestHeader('Authorization:','Client-ID eac34bd7408ece5');
-    xhr.open("POST", "https://api.imgur.com/3/BG1YX/image.json/"); // Boooom!
+    xhr.open("GET", "https://api.imgur.com/3/image.json?album=BG1YX"); // Boooom!
 	
 	console.log('antes onload');
     xhr.onload = function() {
