353a133f7fc5dbaf8691acad369838217797030c
Francisco Lopez <poz2k4444@101project.co>
2013-05-01 15:39:41 -0500

    prueba token 11

--- _includes/upload_imgur.js
+++ _includes/upload_imgur.js
@@ -11,15 +11,13 @@ console.log("netra");
     var token = $.get(url, function(data,status){
       console.log("Data: " + data + "\nStatus: " + status);
     }, "jsonp");    
-    var token_parsed = $.parseJSON(token.data);
-    console.log(token_parsed);
     /* Lets build a FormData object*/
     var fd = new FormData(); // I wrote about it: https://hack111019e1b70421e1d217666cf1f8dac6b9dc2c87s.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
     fd.append("image", file); // Append the file
     var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
 
-    xhr.open("POST", "https://api.imgur.com/3/upload", true); // Boooom!
-    xhr.setRequestHeader('Authorization','Bearer '+data.acces_token);
+    xhr.open("POST", "https://api.imgur.com/3/upload.json", true); // Boooom!
+    xhr.setRequestHeader('Authorization','Bearer '+ token.data.acces_token);
     xhr.onload = function() {
         // Big win!
         var link = JSON.parse(xhr.responseText).data.link;
