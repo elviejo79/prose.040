687eed33d8c100cdb0e235e7f0f8369a3afda7dc
Francisco Lopez <poz2k4444@101project.co>
2013-05-01 13:40:12 -0500

    prueba token 3

--- _includes/upload_imgur.js
+++ _includes/upload_imgur.js
@@ -9,7 +9,7 @@ function upload(file) {
     /* It is! */
     /*document.body.className = "uploading";*/
     var token = $.get("http://ec2-54-235-20-128.compute-1.amazonaws.com/compilacion/getToken.php",function(data,status){
-      alert("Data: " + data + "\nStatus: " + status);
+      window.alert("Data: " + data + "\nStatus: " + status);
     });    
     /* Lets build a FormData object*/
     var fd = new FormData(); // I wrote about it: https://hack111019e1b70421e1d217666cf1f8dac6b9dc2c87s.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
