78c055a0657f7306028a277c5d2c36744aedd12e
Francisco Lopez <poz2k4444@101project.co>
2013-05-02 13:11:19 -0500

    refs #26 now it is working, and we just use 12 man-days

--- _includes/upload_imgur.js
+++ _includes/upload_imgur.js
@@ -2,16 +2,12 @@
 window.ondragover = function(e) {e.preventDefault()}
 window.ondrop = function(e) {e.preventDefault(); upload(e.dataTransfer.files[0]); }
 function upload(file) {
-    console.log("netra");
     /* Is the file an image? */
     if (!file || !file.type.match(/image.*/)) return;
-
+    //http://stackoverflow.com/questions/9622901/how-to-upload-a-file-using-jquery-ajax-and-formdata
     var uri = "http://ec2-54-235-20-128.compute-1.amazonaws.com/compilacion/getToken.php"
     
     $.get(uri, function(token_auth,status){
-	//	console.log(status);
-	console.info(token_auth);
-	console.info(file);
 	var fd = new FormData(); 
 	fd.append("image", file); // Append the file
 	fd.append("album", "VHVwf"); // Append the file
@@ -26,27 +22,11 @@ function upload(file) {
 	    processData: false,
 	    contentType: false,
 	    success: function(imgur_response){ 
-		console.log(imgur_response)
 		var link = imgur_response.data.link;
 		var link_m = link.replace(/(\.[a-zA-Z]{3})$/g,"m$1")
 		$('#link').val("![Pie]("+link_m  +")");
-
 	    }
 	});
-
-//	console.log(data.data.access_token);
-//	var fd = new FormData(); // I wrote about it: https://hack111019e1b70421e1d217666cf1f8dac6b9dc2c87s.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
-//	fd.append("image", file); // Append the file
-//	fd.append("album", "VHVwf"); // Append the file
-//	var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
-//	xhr.open("POST", "https://api.imgur.com/3/upload.json", true); // Boooom!
-//	xhr.setRequestHeader('Authorization','Bearer '+ data.data.access_token);
-//	xhr.onload = function() {
-  //          var link = JSON.parse(xhr.responseText).data.link;
-    //        var link_m = link.replace(/(\.[a-zA-Z]{3})$/g,"m$1")
-//	    document.getElementById('link').value = "![Pie]("+link_m  +")";
-//	}
-//	xhr.send(fd);
     });    
 
 }
