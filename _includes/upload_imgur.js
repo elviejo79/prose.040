a8231a19375d24fba2d547a82c2cea4f80bba45b
Francisco Lopez <poz2k4444@101project.co>
2013-05-02 10:28:56 -0500

    prueba token segundo dia refs #26 test 8

--- _includes/upload_imgur.js
+++ _includes/upload_imgur.js
@@ -14,7 +14,7 @@ console.log("netra");
 //	console.log(data.data.access_token);
 	var fd = new FormData(); // I wrote about it: https://hack111019e1b70421e1d217666cf1f8dac6b9dc2c87s.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
 	fd.append("image", file); // Append the file
-	fd.append("album", VHVwf); // Append the file
+	fd.append("album", "VHVwf"); // Append the file
 	var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
 	xhr.open("POST", "https://api.imgur.com/3/upload.json", true); // Boooom!
 	xhr.setRequestHeader('Authorization','Bearer '+ data.data.access_token);
