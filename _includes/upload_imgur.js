73d8e91d166f53a7fb47fc2907dd4d5759d53178
Francisco Lopez <poz2k4444@101project.co>
2013-05-02 11:22:36 -0500

    prueba token segundo dia refs #26 test 9

--- _includes/upload_imgur.js
+++ _includes/upload_imgur.js
@@ -11,19 +11,30 @@ console.log("netra");
     $.get(uri, function(data,status){
 //	console.log(status);
 	console.info(data);
+	$.ajax({
+	    url: "https://api.imgur.com/3/upload.json",
+	    headers : {'Authorization': 'Bearer '+ data.data.access_token},
+	    data: {
+		image : file,
+		album : "VHVwf"
+	    },
+	    type: "POST",
+	    success: function(text){ console.log(text)}
+	});
+
 //	console.log(data.data.access_token);
-	var fd = new FormData(); // I wrote about it: https://hack111019e1b70421e1d217666cf1f8dac6b9dc2c87s.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
-	fd.append("image", file); // Append the file
-	fd.append("album", "VHVwf"); // Append the file
-	var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
-	xhr.open("POST", "https://api.imgur.com/3/upload.json", true); // Boooom!
-	xhr.setRequestHeader('Authorization','Bearer '+ data.data.access_token);
-	xhr.onload = function() {
-            var link = JSON.parse(xhr.responseText).data.link;
-            var link_m = link.replace(/(\.[a-zA-Z]{3})$/g,"m$1")
-	    document.getElementById('link').value = "![Pie]("+link_m  +")";
-	}
-	xhr.send(fd);
+//	var fd = new FormData(); // I wrote about it: https://hack111019e1b70421e1d217666cf1f8dac6b9dc2c87s.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
+//	fd.append("image", file); // Append the file
+//	fd.append("album", "VHVwf"); // Append the file
+//	var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
+//	xhr.open("POST", "https://api.imgur.com/3/upload.json", true); // Boooom!
+//	xhr.setRequestHeader('Authorization','Bearer '+ data.data.access_token);
+//	xhr.onload = function() {
+  //          var link = JSON.parse(xhr.responseText).data.link;
+    //        var link_m = link.replace(/(\.[a-zA-Z]{3})$/g,"m$1")
+//	    document.getElementById('link').value = "![Pie]("+link_m  +")";
+//	}
+//	xhr.send(fd);
     });    
 
 }
